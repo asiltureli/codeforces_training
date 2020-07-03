eb19a1effe6a4da43fa31081b4349e873b4bc171
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2020-07-03 23:39:28 +0200

    update

---
+++ cpp_800/800_vasya_the_hipster.cpp
@@ -0,0 +1,51 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+using namespace std;
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,n,k) for(int x=k; x < n; ++x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+ 
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int sock1, sock2;
+    cin >> sock1 >> sock2;
+    int min_s = min(sock1,sock2);
+    int max_s = max(sock1,sock2);
+    int counter_1 = min_s;
+    max_s -= min_s;
+    int counter_2 = max_s/2;
+    cout << counter_1 << " " << counter_2;
+
+    return 0;   
+}
