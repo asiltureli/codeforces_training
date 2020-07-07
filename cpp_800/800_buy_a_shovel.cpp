b07ee56a3b5efcfd6ae38cb86e3f94038bf2ff24
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2020-07-07 21:58:29 +0200

    Update

---
+++ cpp_800/800_buy_a_shovel.cpp
@@ -0,0 +1,56 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+using namespace std;
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,k,n) for(int x=k; x < n; ++x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+ 
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int price, coin, amount = 1, add_price;
+    cin >> price >> coin;
+    add_price = price;
+    ford(i,11)
+    {
+        if(add_price%10 == coin || add_price%10 == 0)
+        {
+            break;
+        }
+        add_price += price;
+        amount++;
+    }
+    cout << amount;
+
+    return 0;   
+}
---
+++ cpp_800/800_holiday_of_equality.cpp
@@ -0,0 +1,58 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+using namespace std;
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,k,n) for(int x=k; x < n; ++x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+ 
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int citi, temp;
+    cin >> citi;
+    vector<int> money;
+    ford(k, citi)
+    {
+        cin >> temp;
+        money.push_back(temp);
+    }
+    int max_elem = *max_element(money.begin(), money.end());
+    int result = 0;
+    for(int i:money)
+    {
+        result += max_elem - i;
+    }
+    cout << result;
+
+    return 0;   
+}
