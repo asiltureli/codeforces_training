f83fa1b5d2fa0421abbd8102df798e4ef4df79bd
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2020-07-17 23:15:47 +0200

    Update

---
+++ cpp/800_maximum_in_table.cpp
@@ -0,0 +1,58 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+using namespace std;
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,k,n) for(int x=k; x < n; ++x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+ 
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int cases;
+    cin >> cases;
+    int matrix[11][11];
+    ford(k,11)
+    {
+        matrix[0][k] = 1;
+        matrix[k][0] = 1;
+    }
+    fors(i,1, 11)
+    {
+        fors(ii,1,11)
+        {
+            matrix[i][ii] = matrix[i-1][ii] + matrix[i][ii-1];
+        }
+    }
+    cout << matrix[cases-1][cases-1] << endl;
+    return 0;   
+}
---
+++ cpp/800_most_unstable_array.cpp
@@ -0,0 +1,65 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+using namespace std;
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,k,n) for(int x=k; x < n; ++x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+ 
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int cases, n,m;
+    cin >> cases;
+    ford(k,cases)
+    {
+        cin >> n >> m;
+        if(n < 3)
+        {
+            if(n < 2)
+            {
+                cout << 0 << endl;
+            }
+            else
+            {
+                 cout << m << endl;
+            }
+            
+        }
+        else
+        {
+            cout << m*2 << endl;
+        }
+        
+    }
+    return 0;   
+}
