ce107202ea9683226497db5ffaceb5692a02ff21
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2020-09-03 22:36:03 +0200

    update

---
+++ cpp/900/900_case_of_the_zeros_and_ones_556A.cpp
@@ -0,0 +1,66 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+ 
+#define ford(x,n) for(int x = 0; x < n; ++x)
+#define fors(x,k,n) for(int x=k; x < n; ++x)
+#define forsm(x,k,n) for(int x=k; x > n; --x)
+typedef long long ll;
+typedef pair<int, int> pii;
+ 
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+    int len, n, counter_0 = 0, counter_1 = 0;
+    vector<char> lett;
+    char temp;
+    cin >> len;
+    ford(i, len)
+    {
+        cin >> temp;
+        if(temp == '0')
+        {
+            counter_0++;
+        }
+        else
+        {
+            counter_1++;
+        }
+    }
+    n = abs(counter_0 - counter_1);
+    cout << n;
+    return 0;
+}
