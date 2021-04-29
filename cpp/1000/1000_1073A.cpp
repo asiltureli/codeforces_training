19bea971fe8f3d176505a49252352f9cbc0eeff8
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2021-04-29 14:28:23 +0200

    New solutions

---
+++ cpp/1000/1000_1073A.cpp
@@ -0,0 +1,78 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <locale>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+#include <fstream>
+#include <numeric>
+
+using namespace std;
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+ 
+#define ford(x,end) for(int x = 0; x < end; ++x)
+#define fors(x,start,end) for(int x=start; x < end; ++x)
+#define forsm(x,start,end) for(int x=k; x > n; --x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef pair<string, string> pss;
+const int INF = 1e9;
+const double EPS = 1e-9;
+const double PI = acos(-1.);
+
+int main(int argc, const char * argv[]) {
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+
+    
+    vector<char> cv;
+    char tp;
+    int l;
+    cin >> l;
+    if(l < 2)
+    {
+        cout << "NO";
+        return 0;
+    }
+    ford(i,l)
+    {
+        cin>>tp;
+        cv.push_back(tp);
+        if(i > 0)
+        {
+            if(cv.at(i) != cv.at(i-1))
+            {
+                cout << "YES" << endl;
+                cout << cv.at(i-1) << cv.at(i);
+                return 0;
+            }
+        }
+    }
+    cout << "NO";
+    return 0;
+}
