d1985a522899f1df7c4b9602efd30e9f3eca9fe8
Mert Asil Tuereli <asil.tuereli@rwth-aachen.de>
2021-11-14 22:10:34 +0100

    up

---
+++ cpp/1100/1100_1426C.cpp
@@ -0,0 +1,70 @@
+//  Codeforces_cpp
+//
+//  Created by Mert Asil Türeli
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <locale>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <assert.h>
+#include <cstdio>
+#include <cmath>
+#include <iterator>
+#include <cstring>
+#include <fstream>
+#include <numeric>
+ 
+using namespace std;
+ 
+#define endl '\n'
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+#define ford(x,end) for(int x = 0; x < end; ++x)
+#define fors(x,start,end) for(int x=start; x < end; ++x)
+#define forsm(x,start,end) for(int x=start-1; x >= end; --x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long, long> pll;
+typedef pair<string, string> pss;
+const int INF = 1e9;
+const double EPS = 0e-9;
+const double PI = acos(-1.);
+int calc(int num, int sq)
+{
+    if(num/sq == 1) return (sq-1) ;
+    if(num%sq == 0) return (sq-1) + (num/sq-1) ;
+    else return (sq-1) + (num/sq-1) + 1;
+        
+
+}
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(0);
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); 
+        freopen("output.txt", "w", stdout); 
+    #endif     
+
+
+    int casse, q,d,num;
+    cin >> casse;
+    ford(c,casse)
+    {
+        cin >> num;
+        cout << ceil(2*sqrt(num))-2 << endl;
+    }
+    return 0;
+}
